10a2882a0030e489e9c92664530033c4a4d6a895
Paul Rayes <pjoerayes@live.com>
2015-05-14 19:08:57 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ set -e
 
 wget https://github.com/paulrayes/cloudy-skies/archive/master.zip
 unzip -o master.zip
-mv cloudy-skies-master/.* .
+mv cloudy-skies-master/* .
 rmdir cloudy-skies-master
 rm master.zip
 bundle update
