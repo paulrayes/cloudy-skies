7a9c2ac452ea82980f29a631bfdf467e6b927bad
Paul Rayes <pjoerayes@live.com>
2015-05-14 19:02:53 -0700

    Initial commit

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Exit if any command fails
+set -e
+
+wget https://github.com/paulrayes/cloudy-skies/archive/master.zip
+unzip -o master.zip
+mv cloudy-skies-master/.* .
+rmdir cloudy-skies-master
+rm master.zip
+bundle update
+npm install
